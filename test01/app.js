a6e2f4049284109428925f8d028c83d476269720
byeongseokim <kbs970318@gmail.com>
2023-07-24 18:03:23 +0900

    타입스크립트 기본 설정 및 연습

---
+++ test01/app.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @description 두 수의 합을 구하는 함수
+ * @param {number} a 첫 번째 숫자
+ * @param {number} b 두 번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+var total = sum(200, 300)
+total.toFixed;
